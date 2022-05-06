e3ac8d4c49a5a1180a4b5727d794373ba9313be6
liuxiaoliang <15810140695@163.com>
2022-05-06 14:25:08 +0800

    gateway 过滤器

---
+++ cloud-gateway/gateway-filter/src/main/java/com/ddcode/gateway/GatewayFilterApplication.java
@@ -0,0 +1,12 @@
+package com.ddcode.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayFilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayFilterApplication.class);
+    }
+}
