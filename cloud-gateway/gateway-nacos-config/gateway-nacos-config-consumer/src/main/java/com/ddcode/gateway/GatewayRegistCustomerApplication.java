58294f4016417babb63ac76280c1d204e344bfeb
liuxiaoliang <15810140695@163.com>
2022-05-06 14:08:54 +0800

    gateway整合nacos配置中心

---
+++ cloud-gateway/gateway-nacos-config/gateway-nacos-config-consumer/src/main/java/com/ddcode/gateway/GatewayRegistCustomerApplication.java
@@ -0,0 +1,11 @@
+package com.ddcode.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayRegistCustomerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayRegistCustomerApplication.class);
+    }
+}
---
+++ cloud-gateway/gateway-nacos-config/gateway-nacos-config-route/src/main/java/com/ddcode/gateway/GatewayRegistRouteApplication.java
@@ -0,0 +1,11 @@
+package com.ddcode.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GatewayRegistRouteApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayRegistRouteApplication.class);
+    }
+}
